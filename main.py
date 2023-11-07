7c13a82dcbced28e3159eb96ad42f7839e9f5e35
muganga-charles <mugangacharls5@gmail.com>
2023-11-08 00:05:11 +0300

    fix: organise middleware

--- main.py
+++ main.py
@@ -21,9 +21,17 @@ from py_functions import (
 import py_functions
 
 app = FastAPI()
-origins = (["*"],)
+# origins = (["*"],)
+# app.add_middleware(
+#     CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
+# )
+
 app.add_middleware(
-    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 
