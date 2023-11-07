4e0956417afc9ead094d72c2dad18b67f30c2ba6
muganga-charles <mugangacharls5@gmail.com>
2023-11-07 20:53:13 +0300

    Added Models

--- main.py
+++ main.py
@@ -1,57 +1,36 @@
 from fastapi import FastAPI, HTTPException
 import psycopg2
 from sqlalchemy import create_engine
-# from fastapi.responses import JSONResponse
-# from bcrypt import hashpw, gensalt, checkpw
-# # from email_module import Email
-# # from fastapi import BackgroundTasks
+from fastapi.responses import JSONResponse
+from bcrypt import hashpw, gensalt, checkpw
+
+# from email_module import Email
+# from fastapi import BackgroundTasks
 import config
-# from fastapi.middleware.cors import CORSMiddleware
-# from py_functions import (
-#     Patient,
-#     LoginData,
-#     Hospital,
-#     Doctor,
-#     DoctorLoginData,
-#     HospitalDoctor,
-# )
+from fastapi.middleware.cors import CORSMiddleware
+from py_functions import (
+    Patient,
+    LoginData,
+    Hospital,
+    Doctor,
+    DoctorLoginData,
+    HospitalDoctor,
+)
 
 import py_functions
 
 app = FastAPI()
-# origins = (["*"],)
-# app.add_middleware(
-#     CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
-# )
+origins = (["*"],)
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"]
+)
 
-
-# def connect_db(password):
-#     driver = config.DRIVER_NAME
-#     server = config.SERVER_NAME
-#     database = config.DATABASE_NAME
-#     # vid = config.VID
-#     pwd = password
-#     vid = config.VID
-#     trust = config.TRUST
-#     con_string = (
-#         f"DRIVER={driver};SERVER={server};DATABASE={database};UID={vid};PWD={pwd};"
-#     )
-#     cnxn = pyodbc.connect(con_string)
-#     cnxn.autocommit = True
-#     cursor = cnxn.cursor()
-#     print("Connected to database")
-#     return cnxn
 
 def connect_db():
     server = config.SERVER_NAME
     database = config.DATABASE_NAME
     user = config.USER
     password = config.PASSWORD
-
-    # Create the connection string for psycopg2
-    # con_string = f"dbname='{database}' user='{user}' host='{server}' password='{password}'"
-    # con_string = f"dbname='{database}' user='{user}' host='{server}' password='{password}' port='5432' sslmode='false'"
-    # con_string = f"dbname='{database}' user='SickleSightAdmin@sicklesightserver' host='sicklesightserver.postgres.database.azure.com' password='{password}' port='5432' sslmode='true'"
     con_string = f"dbname='{database}' user='{user}' host='{server}' password='{password}' sslmode='require'"
 
     cnxn = psycopg2.connect(con_string)
@@ -60,154 +39,135 @@ def connect_db():
     print("Connected to database")
     return cnxn
 
+
 cnxn = connect_db()
 
-# def connect_db(password):
-#     driver = config.DRIVER_NAME
-#     server = config.SERVER_NAME
-#     database = config.DATABASE_NAME
-#     vid = config.VID
-#     trust = config.TRUST
-#     # Construct the connection string
-#     params = urllib.parse.quote_plus(
-#         f"DRIVER={driver};SERVER={server};DATABASE={database};UID={vid};PWD={password};TrustServerCertificate={trust};"
-#     )
-    
-#     # Create the SQLAlchemy engine using the connection string
-#     engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
 
-#     print("Connected to database")
-#     return engine
+@app.get("/")
+def get_data():
+    df = py_functions.fetch_data(cnxn)
+    return df.to_dict(orient="records")
 
 
-# cnxn = connect_db(config.PWD)
+@app.post("/patients/new")
+async def create_patient(patient: Patient):
+    # Check if the user exists
+    existing_client = py_functions.existing_patient(
+        cnxn, patient.email, patient.referral_no
+    )
+    if existing_client:
+        raise HTTPException(status_code=400, detail="User already exists.")
+
+    salt_rounds = 12
+    hashed_password = hashpw(patient.password.encode("utf-8"), gensalt(salt_rounds))
+    # hashed_password = ""
+    # Create a new patient with the hashed password
+    patient.password = hashed_password
+
+    py_functions.store_patient(cnxn, patient)
+
+    # Send welcome email
+    # if patient.name:
+    #     await Email(patient.email, "Welcome").send_welcome(patient.name)
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "success": True,
+            "message": "User added successfully.",
+            "data": {"added": True},
+        },
+    )
 
 
-# @app.get("/")
-# def get_data():
-#     df = py_functions.fetch_data(cnxn)
-#     return df.to_dict(orient='records')
+@app.post("/login")
+async def login(login_data: LoginData):
+    # Fetch the patient data from the database using the provided email
+    patient = py_functions.fetch_patient_by_email(cnxn, login_data.email)
+    # print(patient['password'])
+    # If the patient doesn't exist or passwords don't match, return an error
+    # if not patient(
+    if not patient or not checkpw(
+        login_data.password.encode("utf-8"), patient["password"].encode("utf-8")
+    ):
+        raise HTTPException(status_code=401, detail="Invalid email or password.")
+
+    # If everything is okay, return a success message
+    return {
+        "success": True,
+        "message": "Login successful.",
+        "data": {"name": patient["name"]},
+    }
 
 
-# @app.post("/patients/new")
-# async def create_patient(patient: Patient):
-#     # Check if the user exists
-#     existing_client = py_functions.existing_patient(
-#         cnxn, patient.email, patient.referral_no
-#     )
-#     if existing_client:
-#         raise HTTPException(status_code=400, detail="User already exists.")
+@app.post("/hospitals/new")
+async def create_hospital(hospital: Hospital):
+    # Check if the user exists
+    existing_client = py_functions.existing_hospital(cnxn, hospital.Email)
+    if existing_client:
+        raise HTTPException(status_code=400, detail="Hospital already exists.")
 
-#     salt_rounds = 12
-#     hashed_password = hashpw(patient.password.encode("utf-8"), gensalt(salt_rounds))
-#     # hashed_password = ""
-#     # Create a new patient with the hashed password
-#     patient.password = hashed_password
+    py_functions.add_hospital(cnxn, hospital)
 
-#     py_functions.store_patient(cnxn, patient)
+    # Send welcome email
+    # if patient.name:
+    #     await Email(patient.email, "Welcome").send_welcome(patient.name)
 
-#     # Send welcome email
-#     # if patient.name:
-#     #     await Email(patient.email, "Welcome").send_welcome(patient.name)
-
-#     return JSONResponse(
-#         status_code=200,
-#         content={
-#             "success": True,
-#             "message": "User added successfully.",
-#             "data": {"added": True},
-#         },
-#     )
+    return JSONResponse(
+        status_code=200,
+        content={
+            "success": True,
+            "message": "Hospital added successfully.",
+            "data": {"added": True},
+        },
+    )
 
 
-# @app.post("/login")
-# async def login(login_data: LoginData):
-#     # Fetch the patient data from the database using the provided email
-#     patient = py_functions.fetch_patient_by_email(cnxn, login_data.email)
-#     # print(patient['password'])
-#     # If the patient doesn't exist or passwords don't match, return an error
-#     # if not patient(
-#     if not patient or not checkpw(
-#         login_data.password.encode("utf-8"), patient["password"].encode("utf-8")
-#     ):
-#         raise HTTPException(status_code=401, detail="Invalid email or password.")
+@app.post("/doctors/new")
+async def create_doctor(doctor: Doctor):
+    # Check if the user exists
+    existing_client = py_functions.existing_doctor(cnxn, doctor.Email)
+    if existing_client:
+        raise HTTPException(status_code=400, detail="doctor already exists.")
 
-#     # If everything is okay, return a success message
-#     return {
-#         "success": True,
-#         "message": "Login successful.",
-#         "data": {"name": patient["name"]},
-#     }
+    salt_rounds = 12
+    hashed_password = ""
+    # hashed_password = hashpw(doctor.Password.encode("utf-8"), gensalt(salt_rounds))
+    doctor.Password = hashed_password
 
+    py_functions.add_doctor(cnxn, doctor)
 
-# @app.post("/hospitals/new")
-# async def create_hospital(hospital: Hospital):
-#     # Check if the user exists
-#     existing_client = py_functions.existing_hospital(cnxn, hospital.Email)
-#     if existing_client:
-#         raise HTTPException(status_code=400, detail="Hospital already exists.")
+    # Send welcome email
+    # if patient.name:
+    #     await Email(patient.email, "Welcome").send_welcome(patient.name)
 
-#     py_functions.add_hospital(cnxn, hospital)
-
-#     # Send welcome email
-#     # if patient.name:
-#     #     await Email(patient.email, "Welcome").send_welcome(patient.name)
-
-#     return JSONResponse(
-#         status_code=200,
-#         content={
-#             "success": True,
-#             "message": "Hospital added successfully.",
-#             "data": {"added": True},
-#         },
-#     )
+    return JSONResponse(
+        status_code=200,
+        content={
+            "success": True,
+            "message": "doctor added successfully.",
+            "data": {"added": True},
+        },
+    )
 
 
-# @app.post("/doctors/new")
-# async def create_doctor(doctor: Doctor):
-#     # Check if the user exists
-#     existing_client = py_functions.existing_doctor(cnxn, doctor.Email)
-#     if existing_client:
-#         raise HTTPException(status_code=400, detail="doctor already exists.")
+@app.post("/doctors/login")
+async def doctor_login(login_data: DoctorLoginData):
+    # Fetch the patient data from the database using the provided email
+    doctor = py_functions.fetch_doctor_by_email(cnxn, login_data.Email)
+    # print(patient['password'])
+    # If the patient doesn't exist or passwords don't match, return an error
+    # if not doctor(
+    if not doctor or not checkpw(
+        login_data.Password.encode("utf-8"), doctor["Password"].encode("utf-8")
+    ):
+        raise HTTPException(status_code=401, detail="Invalid email or password.")
 
-#     salt_rounds = 12
-#     hashed_password = ""
-#     # hashed_password = hashpw(doctor.Password.encode("utf-8"), gensalt(salt_rounds))
-#     doctor.Password = hashed_password
-
-#     py_functions.add_doctor(cnxn, doctor)
-
-#     # Send welcome email
-#     # if patient.name:
-#     #     await Email(patient.email, "Welcome").send_welcome(patient.name)
-
-#     return JSONResponse(
-#         status_code=200,
-#         content={
-#             "success": True,
-#             "message": "doctor added successfully.",
-#             "data": {"added": True},
-#         },
-#     )
-
-
-# @app.post("/doctors/login")
-# async def doctor_login(login_data: DoctorLoginData):
-#     # Fetch the patient data from the database using the provided email
-#     doctor = py_functions.fetch_doctor_by_email(cnxn, login_data.Email)
-#     # print(patient['password'])
-#     # If the patient doesn't exist or passwords don't match, return an error
-#     # if not doctor(
-#     if not doctor or not checkpw(
-#         login_data.Password.encode("utf-8"), doctor["Password"].encode("utf-8")
-#     ):
-#         raise HTTPException(status_code=401, detail="Invalid email or password.")
-
-#     return {
-#         "success": True,
-#         "message": "Login successful.",
-#         "data": {"name": doctor["DoctorName"]},
-#     }
+    return {
+        "success": True,
+        "message": "Login successful.",
+        "data": {"name": doctor["DoctorName"]},
+    }
 
 
 # # @app.post("/doctor-to-hospital")
@@ -221,4 +181,5 @@ cnxn = connect_db()
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- py_functions.py
+++ py_functions.py
@@ -4,10 +4,12 @@ from pydantic import BaseModel, EmailStr, validator
 from typing import Optional
 import pandas as pd
 
+
 def fetch_data(cnxn):
-    query  = "SELECT TOP 10* FROM PATIENTS"
+    query = "SELECT TOP 10* FROM PATIENTS"
     df = pd.read_sql(query, cnxn)
     return df
+
 
 # Defining Pydantic models for the request body
 class Patient(BaseModel):
@@ -22,25 +24,29 @@ class Patient(BaseModel):
     sex: str  # Assuming 'M' or 'F' for male or female
     password: str
 
+
 class Guardian(BaseModel):
     GuardianName: str
     Relationship: str
     ContactNumber: str
     PatientID: str
 
+
 class LoginData(BaseModel):
     email: EmailStr
     password: str
+
 
 class DoctorLoginData(BaseModel):
     Email: EmailStr
     Password: str
 
+
 class Hospital(BaseModel):
     HospitalName: str
     Address: str
     Country: str
-    Type : str
+    Type: str
     EmergencyLine: str
     HelpLine: str
     RegNumber: str
@@ -49,6 +55,7 @@ class Hospital(BaseModel):
     Docs: str
     ContactNumber: str
     # Password: str/
+
 
 class Doctor(BaseModel):
     DoctorName: str
@@ -63,16 +70,20 @@ class Doctor(BaseModel):
     ContactNumber: str
 
     # Add a validator to check the status value
-    @validator('Status')
+    @validator("Status")
     def check_status(cls, value):
-        if value.lower() not in ['online', 'offline']:
+        if value.lower() not in ["online", "offline"]:
             raise ValueError('Status must be "Online" or "Offline"')
         return value
+
     # Password: str
+
+
 class HospitalDoctor(BaseModel):
     HospitalID: int
     DoctorID: int
     # Password: str
+
 
 class Appointment(BaseModel):
     PatientID: str
@@ -86,6 +97,7 @@ class Appointment(BaseModel):
     Notes: str
     # Password: str
 
+
 class Consultations(BaseModel):
     PatientID: str
     Date: str
@@ -93,6 +105,7 @@ class Consultations(BaseModel):
     DoctorID: str
     Status: str
     Notes: str
+
 
 class Emergency(BaseModel):
     PatientID: str
@@ -105,13 +118,14 @@ class Emergency(BaseModel):
     Notes: str
     # Password: str
 
+
 ## check if the user exists
 def existing_patient(cnxn, email, referral_no):
     query = "SELECT COUNT(1) FROM PATIENTS WHERE Email = ? OR Referral_No = ?;"
     params = (email, referral_no)
     df = pd.read_sql(query, cnxn, params=params)
-    
-    if df.iloc[0, 0] > 0:  
+
+    if df.iloc[0, 0] > 0:
         return True
     else:
         return False
@@ -121,23 +135,23 @@ def existing_patient(cnxn, email, referral_no):
 def store_patient(cnxn, new_patient):
     # Assuming new_patient is a Pydantic model, convert it to a dictionary
     new_patient_data = new_patient.dict()
-    
+
     # Create a cursor object using the connection
     cursor = cnxn.cursor()
-    
+
     # Construct the SQL query with placeholders for parameters
-    placeholders = ', '.join(['?'] * len(new_patient_data))
-    columns = ', '.join(new_patient_data.keys())
+    placeholders = ", ".join(["?"] * len(new_patient_data))
+    columns = ", ".join(new_patient_data.keys())
     sql = f"INSERT INTO PATIENTS ({columns}) VALUES ({placeholders})"
-    
+
     # Execute the SQL query with the provided parameters
     cursor.execute(sql, list(new_patient_data.values()))
-    
+
     # Commit the changes to the database
     cnxn.commit()
     # Close the cursor if you are done with it
     cursor.close()
-    
+
     return True
 
 
@@ -148,24 +162,23 @@ def store_guardian(cnxn, guardian_data):
 
     # Create a cursor object using the connection
     cursor = cnxn.cursor()
-    
+
     # Construct the SQL insert statement with placeholders for parameters
-    placeholders = ', '.join(['?'] * len(guardian_data))
-    columns = ', '.join(guardian_data.keys())
+    placeholders = ", ".join(["?"] * len(guardian_data))
+    columns = ", ".join(guardian_data.keys())
     sql_insert = f"INSERT INTO Guardians ({columns}) VALUES ({placeholders});"
-    
+
     # Execute the SQL insert statement with the provided parameters
     cursor.execute(sql_insert, list(guardian_data.values()))
-    
+
     # Immediately after the insert operation, select the SCOPE_IDENTITY
     cursor.execute("SELECT SCOPE_IDENTITY();")
-    
 
     # Commiting the changes to the database
     cnxn.commit()
 
     cursor.close()
-    
+
 
 # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # def patient_login(cnxn,patient_credentials):
@@ -190,8 +203,9 @@ def store_guardian(cnxn, guardian_data):
 #             return {"success": False, "message": "Invalid email or password."}
 #     else:
 #         return {"success": False, "message": "Invalid email or password."}
-    
+
 #     import pyodbc
+
 
 def fetch_patient_by_email(cnxn, email):
     cursor = cnxn.cursor()
@@ -206,27 +220,31 @@ def fetch_patient_by_email(cnxn, email):
     if result:
         return {
             "name": result[0],
-            "password": result[1]  # Assuming that the password is the second column in the SELECT statement
+            "password": result[
+                1
+            ],  # Assuming that the password is the second column in the SELECT statement
         }
     else:
         return None
-    
+
+
 def existing_hospital(cnxn, email):
     query = "SELECT COUNT(1) FROM Hospitals WHERE Email = ?;"
     params = (email,)
     df = pd.read_sql(query, cnxn, params=params)
-    
-    if df.iloc[0, 0] > 0:  
+
+    if df.iloc[0, 0] > 0:
         return True
     else:
         return False
 
-def add_hospital(cnxn,new_hospital):
+
+def add_hospital(cnxn, new_hospital):
     new_hospital_data = new_hospital.dict()
-    
+
     cursor = cnxn.cursor()
-    placeholders = ', '.join(['?'] * len(new_hospital_data))
-    columns = ', '.join(new_hospital_data.keys())
+    placeholders = ", ".join(["?"] * len(new_hospital_data))
+    columns = ", ".join(new_hospital_data.keys())
     sql = f"INSERT INTO Hospitals ({columns}) VALUES ({placeholders})"
 
     cursor.execute(sql, list(new_hospital_data.values()))
@@ -234,30 +252,33 @@ def add_hospital(cnxn,new_hospital):
     cursor.close()
     return True
 
+
 def existing_doctor(cnxn, email):
     query = "SELECT COUNT(1) FROM Doctors WHERE Email = ?;"
     params = (email,)
     df = pd.read_sql(query, cnxn, params=params)
-    
-    if df.iloc[0, 0] > 0:  
+
+    if df.iloc[0, 0] > 0:
         return True
     else:
         return False
 
-def add_doctor(cnxn,new_doctor):
+
+def add_doctor(cnxn, new_doctor):
     new_doctor_data = new_doctor.dict()
-    
-    new_doctor_data['Status'] = new_doctor_data['Status'].lower() == 'online'
+
+    new_doctor_data["Status"] = new_doctor_data["Status"].lower() == "online"
 
     cursor = cnxn.cursor()
-    placeholders = ', '.join(['?'] * len(new_doctor_data))
-    columns = ', '.join(new_doctor_data.keys())
+    placeholders = ", ".join(["?"] * len(new_doctor_data))
+    columns = ", ".join(new_doctor_data.keys())
     sql = f"INSERT INTO Doctors ({columns}) VALUES ({placeholders})"
 
     cursor.execute(sql, list(new_doctor_data.values()))
     cnxn.commit()
     cursor.close()
     return True
+
 
 def fetch_doctor_by_email(cnxn, email):
     cursor = cnxn.cursor()
@@ -272,10 +293,13 @@ def fetch_doctor_by_email(cnxn, email):
     if result:
         return {
             "DoctorName": result[0],
-            "Password": result[1]  # Assuming that the password is the second column in the SELECT statement
+            "Password": result[
+                1
+            ],  # Assuming that the password is the second column in the SELECT statement
         }
     else:
         return None
+
 
 # def doctor_login(cnxn,doctor_credentials):
 #     if not isinstance(doctor_credentials, dict):
@@ -302,22 +326,22 @@ def fetch_doctor_by_email(cnxn, email):
 
 # def add_hospital_doctor(cnxn, hospital_doctor: HospitalDoctor):
 #     cursor = cnxn.cursor()
-    
+
 #     # Prepare the SQL query to insert the hospital-doctor association
 #     sql = "INSERT INTO HospitalDoctors (HospitalID, DoctorID) VALUES (?, ?);"
-    
+
 #     # Execute the query with the values from the hospital_doctor model
 #     cursor.execute(sql, (hospital_doctor.HospitalID, hospital_doctor.DoctorID))
-    
+
 #     # Commit the changes to the database
 #     cnxn.commit()
-    
+
 #     # Close the cursor
 #     cursor.close()
 
 # def add_hospital_doctor_by_regnumber(cnxn, hospital_doctor_by_regnumber: HospitalDoctorByRegNumber):
 #     cursor = cnxn.cursor()
-    
+
 #     # First, find the HospitalID using the RegNumber
 #     cursor.execute("SELECT HospitalID FROM Hospitals WHERE RegNumber = ?", (hospital_doctor_by_regnumber.RegNumber,))
 #     hospital_id_row = cursor.fetchone()
